34cc13b8f63c6870dc40dceec190769fa88b993a
TayTufenkjian <taytuf@gmail.com>
2022-04-29 11:31:47 -0400

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
 
+    if (firstNum < 0 || secondNum < 0 || typeof firstNum !== 'number' || typeof secondNum !== 'number') {
+        return 'ERROR';
+    }
+
+    let numStart = firstNum;
+    let numEnd = secondNum;
+
+    if (firstNum > secondNum) {
+        numStart = secondNum;
+        numEnd = firstNum;
+    }
+
+    let sum = 0;
+    for (i = numStart; i <= numEnd; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
