9bab340b717f88d036b9a8880de778e11f6a655b
TayTufenkjian <taytuf@gmail.com>
2022-05-01 16:16:07 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    num = parseInt(num);
+
+    if (num < 0) {
+        return 'OOPS';
+    }
+
+    if (num <= 2) {
+        return 1;
+    }
+
+    let sum = 0;
+    let a = 1;
+    let b = 1;
+
+    for (i = 2; i < parseInt(num); i++) {
+        sum = a + b;
+        a = b;
+        b = sum;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
