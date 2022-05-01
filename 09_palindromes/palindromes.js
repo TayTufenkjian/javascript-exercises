3ca9a3b5d8b513d6f95f2e2a49d09ab55400f429
TayTufenkjian <taytuf@gmail.com>
2022-05-01 15:47:57 -0400

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function(inputString) {
 
+    // Find the alpha chars, make them lowercase, add them to an array
+    let myArray = [];
+
+    for (char of inputString) {
+        if (char.match(/\w/)) {
+            myArray.push(char.toLowerCase());  
+        }
+    }
+
+    // Convert the array back to a string 
+    // Reverse the array and convert it to a string 
+    // Compare strings
+    return (myArray.join("") === myArray.reverse().join(""));
 };
 
 // Do not edit below this line
