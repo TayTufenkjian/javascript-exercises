cdc2dba6383b4db1d8dc844545ec733d932c7231
TayTufenkjian <taytuf@gmail.com>
2022-05-01 17:25:48 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    let oldest = people[0];
+
+    for (i = 0; i < people.length; i++) {
+        let person = people[i];
+
+        if (person.yearOfDeath) {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            today = new Date()
+            person.age = today.getFullYear() - person.yearOfBirth;
+        }
+
+        if (person.age > oldest.age) {
+            oldest = person;
+        }
+    }
+
+    return oldest;
 };
 
 // Do not edit below this line
