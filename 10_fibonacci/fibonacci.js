49f02d7e383a00e65b335ecec839f7a15ded9870
TayTufenkjian <taytuf@gmail.com>
2022-05-09 17:24:49 -0400

    Update exercises 10 and 11

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -2,19 +2,14 @@ const fibonacci = function(num) {
 
     num = parseInt(num);
 
-    if (num < 0) {
-        return 'OOPS';
-    }
-
-    if (num <= 2) {
-        return 1;
-    }
+    if (num < 0) return 'OOPS';
+    if (num <= 2) return 1;
 
     let sum = 0;
     let a = 1;
     let b = 1;
 
-    for (i = 2; i < parseInt(num); i++) {
+    for (i = 2; i < num; i++) {
         sum = a + b;
         a = b;
         b = sum;
